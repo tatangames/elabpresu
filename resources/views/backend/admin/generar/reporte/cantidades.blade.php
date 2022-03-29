767f0a1dc766f496f625c0009ed8111e73d59c2b
jonathan moran <tatangamess@gmail.com>
2022-03-29 08:27:29 -0600

    consolidado 1

--- resources/views/backend/admin/generar/reporte/cantidades.blade.php
+++ resources/views/backend/admin/generar/reporte/cantidades.blade.php
@@ -2,25 +2,106 @@
 <head>
     <title>Alcaldía Metapán | Panel</title>
     <style>
-
-
+        body{
+            font-family: Arial;
+        }
         @page {
-            margin: 110px 25px;
+            margin: 145px 25px;
             /* margin-bottom: 10%;*/
         }
-
         header { position: fixed;
             left: 15px;
-            top: -90px;
+            top: -160px;
             right: 0px;
             height: 100px;
             text-align: center;
             font-size: 12px;
         }
+        header h1{
+            margin: 10px 0;
+        }
+        header h2{
+            margin: 0 0 10px 0;
+        }
+        footer {
+            position: fixed;
+            left: 0px;
+            bottom: -10px;
+            right: 0px;
+            height: 10px;
+            /* border-bottom: 2px solid #ddd;*/
+        }
+
+        footer table {
+            width: 100%;
+        }
+        footer p {
+            text-align: right;
+        }
+        footer .izq {
+            margin-top: 20px; !important;
+            margin-left: 20px;
+            text-align: left;
+        }
+
+        .content {
+            padding: 20px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .content img {
+            margin-right: 15px;
+            float: right;
+        }
 
         .content h3{
             font-size: 20px;
 
+        }
+        .content p{
+            margin-left: 15px;
+            display: block;
+            margin: 2px 0 0 0;
+        }
+
+        hr{
+            page-break-after: always;
+            border: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        #tabla {
+            font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            margin-left: 20px;
+            margin-right: 20px;
+            margin-top: 35px;
+            text-align: center;
+        }
+
+        #tabla td{
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: center;
+            font-size: 15px;
+        }
+
+        #tabla th {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: center;
+        }
+
+        #tabla th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            background-color: #f2f2f2;
+            color: #1E1E1E;
+            text-align: center;
+            font-size: 16px;
         }
 
         .fecha{
@@ -28,6 +109,7 @@
             margin-left: 17px;
             text-align: justify;
         }
+
 
 
     </style>
